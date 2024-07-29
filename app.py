3475c8fa32a428bbcac0c4e95194b038f892faee
BreninhoS2 <128260246+BreninhoS2@users.noreply.github.com>
2024-07-29 09:32:32 -0300

    dash
    dash adsh

---
+++ app.py
@@ -0,0 +1,12 @@
+import dash
+
+
+FONT_AWESOME = ["https://use.fontawesome.com/releases/v5.10.2/css/all.css"]
+
+app = dash.Dash(__name__, external_stylesheets=FONT_AWESOME)
+
+app.scripts.config.serve_locally = True
+server = app.server
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
